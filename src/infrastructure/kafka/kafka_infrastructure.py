14b7090bc54a0f3e0aa34bc198aa7336407976f5
Ig0or <igorprovensi@gmail.com>
2023-02-25 23:59:14 -0300

    feat: implemented method to serialize key/and on kafka configs

--- src/infrastructure/kafka/kafka_infrastructure.py
+++ src/infrastructure/kafka/kafka_infrastructure.py
@@ -1,19 +1,24 @@
+# Third Party
+from decouple import config
 from kafka import KafkaProducer
 
-from decouple import config
+# Local
+from src.services.obfuscate_data.obfuscate_data_service import ObfuscateDataService
+
 
 class KafkaInfrastructure:
     __producer = None
 
     @classmethod
-    async def get_producer(cls) -> KafkaProducer:
+    def get_producer(cls) -> KafkaProducer:
         if cls.__producer is None:
             producer_config = {
                 "bootstrap_servers": config("KAFKA_URL"),
-                "client_id": config("CLIENT_ID")
+                "client_id": config("CLIENT_ID"),
+                "key_serializer": ObfuscateDataService.obfuscate_value,
+                "value_serializer": ObfuscateDataService.obfuscate_value
             }
 
             cls.__producer = KafkaProducer(**producer_config)
 
         return cls.__producer
-
