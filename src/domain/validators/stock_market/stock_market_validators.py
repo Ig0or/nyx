a453ad6aeaff16926008a2cb4b659a857043f2ac
Ig0or <igorprovensi@gmail.com>
2023-03-06 15:33:52 -0300

    fix: quantity and price check on validator

--- src/domain/validators/stock_market/stock_market_validators.py
+++ src/domain/validators/stock_market/stock_market_validators.py
@@ -1,5 +1,5 @@
 # Third Party
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 # Local
 from src.domain.enums.stock_market.stock_market_enums import OrdersTypeEnum
@@ -10,3 +10,17 @@ class StockOrderValidator(BaseModel):
     quantity: int
     price: float
     order_type: OrdersTypeEnum
+
+    @validator("quantity")
+    def quantity_check(cls, quantity: int):
+        if quantity <= 0:
+            raise ValueError("Quantity must be greater than 0.")
+
+        return quantity
+
+    @validator("price")
+    def price_check(cls, price: int):
+        if price <= 0:
+            raise ValueError("Price must be greater than 0.")
+
+        return price
