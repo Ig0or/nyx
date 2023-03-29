2d09d07c52652ca0b840cf73a6af33959c8cbbc4
Ig0or <igorprovensi@gmail.com>
2023-03-29 09:59:26 -0300

    feat: changes on main file

--- nyx.py
+++ nyx.py
@@ -1,11 +1,30 @@
 # Third Party
+from decouple import config
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pyfiglet import print_figlet
 import uvicorn
 
 # Local
 from src.routers.base.base_router import BaseRouter
 
-app = BaseRouter.register_routers()
+
+def build_app() -> FastAPI:
+    app = BaseRouter.register_routers()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
 
 
 if __name__ == "__main__":
-    uvicorn.run(app=app)
+    app = build_app()
+
+    print_figlet(text="Nyx", colors="0;0;255")
+
+    port = int(config("SERVER_PORT"))
+    uvicorn.run(app=app, host="0.0.0.0", port=port)
